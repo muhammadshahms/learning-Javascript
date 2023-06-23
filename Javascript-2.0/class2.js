92b7e50f8caece46302d0dc1876d991067f2fd71
muhammadshahms <muhammadshah516@gmail.com>
2023-06-23 19:20:44 +0500

    .

---
+++ Javascript-2.0/class2.js
@@ -0,0 +1,19 @@
+// Primitive
+
+// x = "s"
+// console.log(typeof(x));
+
+// a = 78
+// console.log(typeof(a))
+
+// a=null
+// console.log(typeof(a));
+
+// a= NaN  
+// console.log(typeof(a));
+
+a=[]
+console.log(typeof(a));
+
+a= {}
+console.log(typeof(a));
