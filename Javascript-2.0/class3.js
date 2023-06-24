fe5ffbc8d42cb5a54d74d5216b5fee07d6cb45fe
muhammadshahms <muhammadshah516@gmail.com>
2023-06-24 20:59:43 +0500

    .

---
+++ Javascript-2.0/class3.js
@@ -0,0 +1,30 @@
+// function add(x,y){
+//     console.log(x+y);
+// }
+// console.log(add(5,6));
+// 54
+
+
+
+// function dosomething () {
+//     let a="something"
+//     return a
+//   }
+// //   console.log(dosomething())
+//   function how() {
+//     return dosomething()
+    
+//   }
+
+//   console.log(how())
+
+
+const sum = (a,b) => {
+   return a + b
+}
+// sum(5, 6);
+
+// const sum = function(a,b){
+//     return a+b
+// } 
+console.log(sum(6,7));
