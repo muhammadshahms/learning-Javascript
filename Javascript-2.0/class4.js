da9195797c58ef09e621e435e8efde56d1f25a77
muhammadshahms <muhammadshah516@gmail.com>
2023-06-27 20:48:44 +0500

    .

--- Javascript-2.0/class4.js
+++ Javascript-2.0/class4.js
@@ -1,10 +1,17 @@
-const car = {
-    carName:"BMW",
-    model:"X5",
-    engineType:"v8",
-    color:"black",
-    quantity:()=>{
-        console.log("quantity123123213");}
+const showRoom = function() {
+    const car = {
+        carName: "BMW",
+        model: "X5",
+        engineType: "v8",
+        color: "black",
+        getobj: function () {
+            return `Your car name is ${this.carName},
+your car model is ${this.model},
+your car engine type is ${this.engineType},
+your car color is ${this.color}.
+`   }
+    }
+    console.log(car.getobj());
 }
 
-console.log(car.quantity());
+showRoom()
