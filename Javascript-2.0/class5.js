d6b041cf3ea6e1608927e8f60eca24d40a5906ae
Shah <muhammadshah516@gmail.com>
2023-07-01 12:58:12 +0500

    .

--- Javascript-2.0/class5.js
+++ Javascript-2.0/class5.js
@@ -1,15 +1,15 @@
 //how to use this function
 
-const jeff = {
-    face: "@",
-    whodis: function () {
-        console.log(this)
-    },
-    butWhoAmI: () => {
-        console.log(this)
-    }
-}
-console.log(jeff.whodis())
+// const jeff = {
+//     face: "@",
+//     whodis: function () {
+//         console.log(this)
+//     },
+//     butWhoAmI: () => {
+//         console.log(this)
+//     }
+// }
+// console.log(jeff.whodis())
 
 
 // Since fat arrow functions do not have their own this binding,
@@ -18,3 +18,20 @@ console.log(jeff.whodis())
 //However, it's important to note that using a fat arrow function is not always appropriate, 
 // especially when you need dynamic binding of this 
 // based on the function invocation context.
+
+// function Horse(name) {
+//     this.name = name;
+//     this.poop = function () {
+//         console.log('00');
+//         return this
+//     }
+// }
+
+// const myHorse = new Horse('Secretariat');
+// console.log(myHorse.poop())
+
+
+// spread sytax
+const array = ["hello", "hye", "bye"];
+const obj = { ...array };
+console.log(obj)
