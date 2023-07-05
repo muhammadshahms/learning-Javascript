db698d2a9821fffb651ef39e15481524bb477d1b
muhammadshahms <muhammadshah516@gmail.com>
2023-07-05 20:53:34 +0500

    .

--- Javascript-2.0/class4.js
+++ Javascript-2.0/class4.js
@@ -19,3 +19,4 @@ const showRoom = function () {
     }
 }
 showRooms()
+showRoom()
--- Javascript-2.0/roughWork.js
+++ Javascript-2.0/roughWork.js
@@ -1,3 +1,3 @@
-import { add } from "./class5";
+import add from "./class5";
 const adding=add(2,3)
 console.log(adding)
