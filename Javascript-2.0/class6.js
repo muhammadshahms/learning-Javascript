8d0854187eed1487de91207b252b280960facb5a
muhammadshahms <muhammadshah516@gmail.com>
2023-07-06 20:29:17 +0500

    .

---
+++ Javascript-2.0/class6.js
@@ -0,0 +1,34 @@
+// const table = (input) => {
+//     input = 5
+//     for (let i = 0; i <= 10; i++) {
+//         const element = i * input;
+//         console.log(element);
+//     }
+// }
+// table(5)
+
+// array = [1,4,5,6,7,6,6]
+// // console.log(array.length)
+// for (let index = 0; index < array.length; index++) {
+    
+//     const element = array[index];
+//     console.log(element)
+// }
+
+// const a = 10
+// const b = 9
+// const c = 8
+// const d = 7
+// const e = 6
+
+const {a,b,c,d,e} = {a:10,b:9,c:8,d:7,e:6}
+console.log(a)
+
+
+
+// var a ="jhello"
+// {
+//     var a="kia hoa"
+    
+// }
+// console.log(a);
--- Javascript-2.0/roughWork.js
+++ Javascript-2.0/roughWork.js
@@ -1,3 +1,3 @@
-import add from "./class5";
+import {add} from "./class5.js";
 const adding=add(2,3)
 console.log(adding)
