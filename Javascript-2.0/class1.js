54ab316764cf3ed9e7137dcfcc33584f2109abf0
muhammadshahms <muhammadshah516@gmail.com>
2023-07-17 18:57:16 +0500

    .

--- Javascript-2.0/class1.js
+++ Javascript-2.0/class1.js
@@ -3,7 +3,34 @@
 // z = x + y
 // console.log(z);
 
+// var x = 5 //declare,asssign, store
+// var x= 7
+// var y=6
+// var y=9
+// var z = x+y
+// console.log(z)
 
+
+const y; //can't declare
+y=1
+y=0
+console.log(y);
+
+let x=0; //declaration
+x=9
+x=0
+x=1
+x=1
+console.log(x);
+
+
+// const x= 6 //store,assign value
+// const y =7
+// const z = x+y
+// console.log(z);
+
+
+//let and const can't be reassigned other then that var can be assigned multiple
 
 // var x = 6
 // var x = 1
@@ -78,8 +105,8 @@
 // console.log(a);
 
 
-let x;
-x=10;
-x=9
-x=1000
-console.log(x);
+// let x;
+// x=10;
+// x=9
+// x=1000
+// console.log(x);
