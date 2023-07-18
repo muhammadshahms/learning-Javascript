4b7a809941f533bc00f9b774e10e5ac63e585001
Shah <muhammadshah516@gmail.com>
2023-07-18 22:37:00 +0500

    .

--- Javascript-2.0/class10.js
+++ Javascript-2.0/class10.js
@@ -1,9 +1,9 @@
 const promise = fetch('https://jsonplaceholder.typicode.com/posts')
 
 promise.then(res => res.json())
-.then(data => {
-    console.log('hey',data[0].title);
-})
-.catch(err => console.log(err))
 
-console.log('sync');
+.then(data => {
+    return data
+})
+.then(data => console.log('hey',data[0].title))
+.catch(err => console.log(err))
