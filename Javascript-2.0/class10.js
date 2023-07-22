315b88b55b75634685b4d0fd7400a54abea8c9ee
muhammadshahms <muhammadshah516@gmail.com>
2023-07-22 19:58:58 +0500

    .

--- Javascript-2.0/class10.js
+++ Javascript-2.0/class10.js
@@ -1,3 +1,4 @@
+//get method
 const promise = fetch('https://jsonplaceholder.typicode.com/todos')
 promise.then(res => res.json())
 
@@ -6,5 +7,3 @@ promise.then(res => res.json())
 })
 .then(data => console.log('hey',data[0].title))
 .catch(err => console.log(err))
-
-console.log('sync');
