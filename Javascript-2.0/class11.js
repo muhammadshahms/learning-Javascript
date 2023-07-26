2cd154b9ee7e23e788bdbf9ab417993bbcc13a71
muhammadshahms <muhammadshah516@gmail.com>
2023-07-26 18:27:57 +0500

    .

--- Javascript-2.0/class11.js
+++ Javascript-2.0/class11.js
@@ -1,22 +1,54 @@
-async function fetchData() {
-    const response = await fetch('https://fakestoreapi.com/products',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: 'soap',
-                description: 'silky soap',
-                price: 200
-            })
-        }
-    )
-    const data = await response.json()
-    console.log(data) 
-  
+
+// let json_data={
+//     title: 'soap',
+//     description: 'silky soap',
+//     price: 200
+// }
+// async function fetchData() {
+//     const response = await fetch('https://fakestoreapi.com/products',
+//         {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(json_data)
+//         }
+//     )
+//     const data = await response.json()
+//     console.log(data) 
+
+// }
+
+// // async me .then ki jagah await lagate hain or .then bhi laga skte ho
+
+// fetchData();
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//         console.log(JSON.stringify(data));
+//     })
+//     .catch(err => console.log(err));
+
+
+
+
+
+async function promiss(){
+const prom= await fetch('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    header:{
+        'Content-Type':"application/json"
+    },
+    body:JSON.stringify({
+        title:'soap',
+        description:'silky soap',
+        price:200
+    })
+})
+
+const _data =await prom.json()
+console.log(_data);
 }
-
-// async me .then ki jagah await lagate hain or .then bhi laga skte ho
-
-fetchData();
+promiss()
--- Javascript-2.0/class3.js
+++ Javascript-2.0/class3.js
@@ -1,3 +1,32 @@
+// // increament
+// let i=9
+// i++
+// console.log(i);
+
+// let i=0;initialize
+// i<=10; condition
+// i++; increament // statement
+
+
+
+for (var x=0; x<=15; x++) {
+   if (x % 2 === 0) {
+           console.log(x + " is even");   
+   }
+   else {
+           console.log(x + " is odd");
+   }
+}
+
+
+
+
+// for(Init;Cond;statement){
+//    statement
+// }
+
+
+
 // function add(x,y){
 //     console.log(x+y);
 // }
@@ -18,21 +47,21 @@
 //   console.log(how())
 
 
-const sum = (a,b) => {
-   return a + b
-}
-// sum(5, 6);
+// const sum = (a,b) => {
+//    return a + b
+// }
+// // sum(5, 6);
 
-// const sum = function(a,b){
-//     return a+b
-// } 
-console.log(sum(6,7));
+// // const sum = function(a,b){
+// //     return a+b
+// // } 
+// console.log(sum(6,7));
 
-// anonymus funnction or fat arrow function
+// // anonymus funnction or fat arrow function
 
 
-const car = {
-   make: 'Toyota',
-    model: 'Corolla',
-    year: 2020,
-}
+// const car = {
+//    make: 'Toyota',
+//     model: 'Corolla',
+//     year: 2020,
+// }
